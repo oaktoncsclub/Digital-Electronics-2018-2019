4af42802d865b6ec0c22df2cd393a0a27eb5b4ae
Andrew L <li_andrewy@protonmail.com>
2019-03-26 16:06:06 -0400

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -2,22 +2,26 @@
 #define ONBOARD_LED 13
 enum Interval {
   kIntervalMinimumConfirmed = 10, //ms
-  kIntervalHumanObservable = 100  //ms
+  kIntervalHumanObservable = 100,  //ms
+  kTest = 20
 };
 
 /// put your setup code here, to run once:
 void setup() {
   pinMode(EXTERNAL_LED, OUTPUT);
   pinMode(ONBOARD_LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 /// put your main code here, to run repeatedly:
 void loop() {
+  unsigned long t1 = millis();
   digitalWrite(EXTERNAL_LED, HIGH);
   digitalWrite(ONBOARD_LED, HIGH);
-  delay(kIntervalHumanObservable);
+  delay(kTest);
   
   digitalWrite(EXTERNAL_LED, LOW);
   digitalWrite(ONBOARD_LED, LOW);
-  delay(kIntervalHumanObservable);
+  delay(kTest);
+  Serial.println(millis() - t1);
 }
